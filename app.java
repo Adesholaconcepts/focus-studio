0b4dbd7bda17105ed2f419d6535c2348d29a3e13
Adeshola David <adesholababafemi@yahoo.com>
2023-05-10 14:42:37 +0000

    first commit

---
+++ app.java
@@ -0,0 +1,7 @@
+# This is me just trying to be better.
+# i will certainly be better than i have ever been. Because God is with me
+# and his Rod and staff they comfort me so well that i in turn comfort others.
+#
+# Please do not hard code
+# make use of variable
+# author: Adeshola David
